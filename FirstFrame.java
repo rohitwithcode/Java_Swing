f60fc5f157d33f5150a46d87cde9340c0e7ad1f7
Rohit Kumar <145838244+rohitwithcode@users.noreply.github.com>
2023-10-12 21:10:33 +0530

    JFrame added

---
+++ FirstFrame.java
@@ -0,0 +1,13 @@
+import javax.swing.*;
+
+public class FirstFrame{
+	public static void main(String [] args){
+		
+		JFrame frame =new JFrame();
+		
+		frame.setVisible(true);
+		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		frame.setSize(400,400);
+		frame.setLocation(100,50);
+}
+}
