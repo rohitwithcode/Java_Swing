74db416cd60e06de570864b92df872c41256cc2b
Rohit Kumar <145838244+rohitwithcode@users.noreply.github.com>
2023-10-13 21:25:14 +0530

    My FirstFrame added

--- FirstFrame.java
+++ FirstFrame.java
@@ -1,5 +1,5 @@
 import javax.swing.*;
-
+import java.awt.*;
 public class FirstFrame{
 	public static void main(String [] args){
 		
@@ -7,7 +7,22 @@ public class FirstFrame{
 		
 		frame.setVisible(true);
 		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-		frame.setSize(400,400);
-		frame.setLocation(100,50);
+		//frame.setSize(400,400);
+		//frame.setLocation(100,50);
+
+		frame.setBounds(100,100,400,400);
+		frame.setTitle("My First Frame");
+
+		// ImageIcon icon=new ImageIcon("Path set");
+		// frame.setIconImage(icon.Image());
+
+		Conatainer c=frame.getContentPane();
+
+		// Color color=new Color(255,0,0);
+
+		c.setBackgroun(Color.RED);
+
+		// frame.setVisible(false);
+		
 }
 }
